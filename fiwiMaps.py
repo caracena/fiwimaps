01f2ddaadc38bd6add953025c21d2a3a0ea7f683
caracena <claudio.aracena2@gmail.com>
2016-01-11 17:26:51 +1100

    Create matrix files

--- fiwiMaps.py
+++ fiwiMaps.py
@@ -1,8 +1,14 @@
 import sys
 from saliency_map import SaliencyMap
 from utils import OpencvIo
+from os import listdir
+from os.path import isfile, join
+import numpy as np
 
-oi = OpencvIo()
-src = oi.imread("stimuli/agoda.png")
-sm = SaliencyMap(src)
-oi.imshow_array([sm.map])
+onlyfiles = [f for f in sorted(listdir('stimuli')) if isfile(join('stimuli', f))]
+
+for f in onlyfiles:
+        oi = OpencvIo()
+        src = oi.imread("stimuli/"+f)
+        sm = SaliencyMap(src)
+        np.savetxt('results/'+f+'.txt',sm.map)

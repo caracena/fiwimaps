a0ea42557f3707e8e15a55b165ddfc75d355b533
caracena <claudio.aracena2@gmail.com>
2016-01-26 18:15:49 +1100

    add logging

--- fiwiMaps.py
+++ fiwiMaps.py
@@ -6,9 +6,12 @@ from os.path import isfile, join
 import numpy as np
 
 onlyfiles = [f for f in sorted(listdir('stimuli')) if isfile(join('stimuli', f))]
+oi = OpencvIo()
 
+## option 1: using saliency_map.py
 for f in onlyfiles:
-        oi = OpencvIo()
-        src = oi.imread("stimuli/"+f)
-        sm = SaliencyMap(src)
-        np.savetxt('results/'+f+'.txt',sm.map)
+    print('Doing saliency map of file {}'.format(f))
+    src = oi.imread("stimuli/"+f)
+    sm = SaliencyMap(src)
+    np.savetxt('results/'+f+'.txt',sm.map)
+    print('Saliency map of file {} ready'.format(f))
